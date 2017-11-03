bb8645f18f33a488b995519ca44ede7c14c3a7c0
Mattias Asplund <mattias@mattiasasplund.se>
2017-11-03 07:21:17 +0100

    Add project files.

---
+++ ChainOfResponsibilityWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChainOfResponsibilityWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
